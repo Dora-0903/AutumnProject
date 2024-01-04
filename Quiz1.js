d232ed716750f5c3e6cbcfb1ffe12e5c9e1c6f63
Dora-0903 <131092027+Dora-0903@users.noreply.github.com>
2024-01-04 23:19:19 +0900

    Update Quiz1.js

--- Quiz1.js
+++ Quiz1.js
@@ -1,4 +1,4 @@
-function Q1() {if (document.getElementById('answer1').value == 'どこでもドア')
+function quiz1() {if (document.getElementById('answer1').value == 'どこでもドア')
                   {alert('正解！);}
                   else{alert('はずれ！');}
                   }
