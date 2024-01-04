50f192f689f2d480b5743b4e8724e3496bf97f42
Dora-0903 <131092027+Dora-0903@users.noreply.github.com>
2024-01-04 23:53:26 +0900

    Update Quiz-1.js

--- Quiz-1.js
+++ Quiz-1.js
@@ -1,4 +1,4 @@
 function quiz1() {if (document.getElementById('answer1').value == 'どこでもドア')
-                  {alert('正解！);}
+                  {alert('正解！');}
                   else{alert('はずれ！');}
                   }
