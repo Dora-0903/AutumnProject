e26b4311bf1fa16eea0e4d2798e4da9ae52aa952
Dora-0903 <131092027+Dora-0903@users.noreply.github.com>
2024-01-06 23:54:06 +0900

    Update Quiz-3.js

--- Quiz-3.js
+++ Quiz-3.js
@@ -3,18 +3,18 @@ function quiz3() {if (document.getElementById('answer3').value == 'スペアポ�
                   else{alert('はずれ！');}
                   }
 
-function hyoji13(){
+function hyoji2(){
   //表示する文字　↓
-var str13 = "一文字ずつ表示します。";
+var str = "四次元ポケットじゃないよ！";
   //テキストボックスの文字数　↓
-var cnt13 = document.timer13.moji13.value.length;
+var cnt = document.timer.moji.value.length;
   //文字数が全部表示されているか確認　↓
-if (cnt13 < 11) {
+if (cnt < 13) {
   //現在より１文字多く切り出して表示　↓
-  document.timer13.moji13.value = str13.substr(0,cnt13+1);}
+  document.timer.moji.value = str.substr(0,cnt+1);}
   else{
     //全て表示されていたら、空文字に戻す
-  document.timer13.moji13.value = "";}
+  document.timer.moji.value = "";}
   //setTimeout()を含む関数を呼び出す
-  setTimeout("hyoji13()",1000);}
+  setTimeout("hyoji2()",1000);}
 
